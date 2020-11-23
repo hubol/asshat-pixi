c140feed4ac344fac98d00d798169671e1e07c8d
Hubol <hubol.gordon@gmail.com>
2020-11-23 01:48:20 -0600

    More demo

--- example/index.ts
+++ example/index.ts
@@ -1,10 +1,19 @@
 import {createGame} from "../src/asshat/createGame";
 import {Graphics} from "pixi.js";
 import {Key} from "../src/utils/browser/key";
+import {now} from "../src/utils/now";
 
 const game = createGame({width: 640, height: 480, targetFps: 60});
 game.canvasElement.id = "gameCanvas";
 document.body.appendChild(game.canvasElement);
+
+const lines = new Graphics()
+    .lineStyle(1, 0x808080)
+    .withStep(() => {
+        const x = game.width * (Math.sin(now.ms * 0.125) + 1) / 2;
+        const y = game.height * (Math.cos(now.ms * 0.5) + 1) / 2;
+        lines.lineTo(x, y);
+    });
 
 const circle = new Graphics()
     .withStep(() => {
@@ -13,6 +22,11 @@ const circle = new Graphics()
             circle.x += 8;
             circle.y += 8;
         }
+        else if (Key.isDown("Backspace"))
+        {
+            circle.x -= 4;
+            circle.y -= 4;
+        }
 
         circle
             .clear()
@@ -20,4 +34,4 @@ const circle = new Graphics()
             .drawCircle(0, 0, 32)
     });
 
-game.stage.addChild(circle);
+game.stage.addChild(lines, circle);
