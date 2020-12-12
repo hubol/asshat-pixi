ad99c60f1a2c59482f664e806d4b1ae1d70bae43
Hubol <hubol.gordon@gmail.com>
2020-12-12 01:13:23 -0600

    Add integralUpscaleCanvas.ts

--- src/game/game.ts
+++ src/game/game.ts
@@ -7,10 +7,12 @@ import {Jukebox} from "../utils/jukebox";
 import {Iguana} from "../typedAssets/textures";
 import {CratePickup} from "../typedAssets/sounds";
 import {Fly} from "../typedAssets/musics";
+import {integralUpscaleCanvas} from "../utils/browser/integralUpscaleCanvas";
 
 const game = createGame({width: 640, height: 480, targetFps: 60});
 game.canvasElement.id = "gameCanvas";
 document.body.appendChild(game.canvasElement);
+integralUpscaleCanvas(game.canvasElement, 20);
 
 const lines = new Graphics()
     .withStep(() => {
